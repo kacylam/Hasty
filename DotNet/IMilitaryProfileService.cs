79ebd5c6782e991d125291e232cdcb18d607d080
kacylam <117318149+kacylam@users.noreply.github.com>
2023-03-20 12:55:40 -0700

    Add files via upload

---
+++ DotNet/IMilitaryProfileService.cs
@@ -0,0 +1,16 @@
+﻿using Sabio.Models;
+using Sabio.Models.Domain.MilitaryProfile;
+using Sabio.Models.Requests.MilitaryProfiles;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface IMilitaryProfileService
+    {
+        int Add(MilitaryProfileAddRequest model, int userId);
+        void Delete(int Id);
+        MilitaryProfile Get(int id);
+        Paged<MilitaryProfile> GetByPage(int pageIndex, int pageSize);
+        Paged<MilitaryProfile> SearchPaginated(int pageIndex, int pageSize, string query);
+        void Update(MilitaryProfileUpdateRequest model, int userId);
+    }
+}
---
+++ DotNet/MilitaryProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.MilitaryProfile
+{
+    public class MilitaryProfile
+    {
+        public int Id { get; set; }
+        public BaseUser User { get; set; }
+        public string Email { get; set; }
+        public int MonthlyIncome { get; set; }
+        public DateTime MoveInDate { get; set; }
+        public bool IsActiveDuty { get; set; }
+        public LookUp Branch { get; set; }
+        public LookUp Rank { get; set; }
+        public bool HasHousingAllowance { get; set; }
+        public string Unit { get; set; }
+        public DateTime NoLaterThanDate { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
